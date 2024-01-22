d852cb4d6e9b1d296af80fc84ca50ba13c033727
Divyansh Singh <115107879+Div9411@users.noreply.github.com>
2024-01-22 22:25:57 +0530

    Update supabase.js

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabaseUrl = "https://kteohcvyjpwbpuvqcqzs.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt0ZW9oY3Z5anB3YnB1dnFjcXpzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkwMDUxMzYsImV4cCI6MjAxNDU4MTEzNn0.ivhPP0pN-3EzFvkDoiKuh4VeRPQALJUQJGVFG9JZy68";
+const supabaseKey ='api_key';    //hiding my original key for security purposes
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
